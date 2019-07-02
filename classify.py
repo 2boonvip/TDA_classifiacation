130a108c379240347e73e9e7aa53e38c930f42a5
Shintaro Fujiwara <2boonvip@gmail.com>
2019-07-02 13:28:19 +0900

    commit

--- classify.py
+++ classify.py
@@ -21,24 +21,24 @@ X = (X - np.amin(X,axis=0)) /( np.amax(X,axis=0))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-def create_single_module(input_tensor, output_channel):
-    x = Conv1D(output_channel, kernel_size=3)(input_tensor)
+def create_single_module(input_tensor, output_channel,size):
+    x = Conv1D(output_channel, kernel_size=size)(input_tensor)
     x = BatchNormalization()(x)
     return Activation("softmax")(x)
 
 
 #モデル作成
-input = Input(shape=(300, 1)) # 384dim
-x = create_single_module(input, 60) # 30->28 dim
-x = MaxPool1D(5)(x) # 28 -> 14dim
-x = create_single_module(x, 12) # 14 -> 12
-x = MaxPool1D(2)(x) # 10 -> 5
+input = Input(shape=(300, 1)) 
+x = create_single_module(input, 60,4) 
+x = MaxPool1D(5)(x)
+x = create_single_module(x, 12,2)
+x = MaxPool1D(2)(x)
 x = Flatten()(x)
 x = Dense(6, activation="softmax")(x)
 
 model = Model(input, x)
 model.compile(Adam(lr=1e-3), loss="binary_crossentropy", metrics=["acc"])
-history = model.fit(X_train, y_train, batch_size=32, epochs=100, validation_data=(X_train, y_train)).history
+history = model.fit(X_train, y_train, batch_size=32, epochs=100, validation_data=(X_test, y_test)).history
 
 max_val_acc = max(history["val_acc"])
 plt.plot(np.arange(100)+1, history["val_acc"])
